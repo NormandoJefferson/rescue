6355031e85cf398977b23a9fd1b3371a3f67aa81
NormandoJefferson <normandojefferson@hotmail.com>
2022-04-23 10:35:15 -0300

    Explosao do amigo

--- js/js.js
+++ js/js.js
@@ -232,6 +232,17 @@ function start() {
 
 			$("#amigo").remove();
 		}
+
+		//Inimigo2 com o amigo.
+		
+		if (colisao6.length>0) {	
+			amigoX = parseInt($("#amigo").css("left"));
+			amigoY = parseInt($("#amigo").css("top"));
+			explosao3(amigoX,amigoY);
+			$("#amigo").remove();
+					
+			reposicionaAmigo();		
+		}
 	} 
 
 	//Explosão 1.
@@ -271,7 +282,21 @@ function start() {
 			window.clearInterval(tempoExplosao2);
 			tempoExplosao2=null;
 		}	
-	} 
+	}
+	
+	//Explosão3.
+	function explosao3(amigoX,amigoY) {
+		$("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+		$("#explosao3").css("top",amigoY);
+		$("#explosao3").css("left",amigoX);
+		var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+		
+		function resetaExplosao3() {
+			$("#explosao3").remove();
+			window.clearInterval(tempoExplosao3);
+			tempoExplosao3=null;	
+		}	
+	}
 
 	//Reposiciona Inimigo2.
 	function reposicionaInimigo2() {
